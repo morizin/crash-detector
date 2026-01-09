c7fad7e8aac51a5cc473f45c72b76ab5f19c6639
Mohammed Rizin <mrizin2013@gmail.com>
2026-01-09 15:30:40 +0530

    Logging setup

---
+++ src/crash_detection/__init__.py
@@ -0,0 +1,26 @@
+import logging
+from dotenv import load_dotenv
+from datetime import datetime
+import os
+import sys
+
+load_dotenv()
+PROJECT_NAME = __name__.split(".")[-1]
+__version__ = "0.1.0"
+
+logging_str = "%(asctime)s [%(levelname)s] : %(module)s - %(message)s"
+
+timestamp = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=logging_str,
+    handlers=[
+        logging.FileHandler(os.path.join(LOG_DIR, f"{timestamp}.log")),
+        logging.StreamHandler(sys.stdout),
+    ],
+)
+
+logger = logging.getLogger("CrashDetectionLogger")
